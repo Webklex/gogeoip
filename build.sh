be3856c86883a3d7d60d738495ea041f2177ed4b
webklex <info@webklex.com>
2022-09-01 00:20:44 +0200

    New build flags added

--- build.sh
+++ build.sh
@@ -9,7 +9,7 @@ BUILD_NUMBER=$(date -u '+%Y%m%d.%H%M.%S')
 BUILD_COMMIT=$(git rev-list -1 HEAD)
 BUILD_VERSION=$(git tag | tail -1)
 
-BINARY_NAME="geoip"
+BINARY_NAME="gogeoip"
 
 function pack() {
 	PACKAGE_NAME=${BINARY_NAME}-${BUILD_VERSION}-${1}
@@ -24,12 +24,15 @@ function pack() {
 	sha256sum ${OUTPUT_BINARY} | sed 's/\ .*\// /g' >> ${PACKAGE_DIR}/sha256.hash
 	sha512sum ${OUTPUT_BINARY} | sed 's/\ .*\// /g' >> ${PACKAGE_DIR}/sha512.hash
 
+	echo "MD5    $(cat ${PACKAGE_DIR}/md5.hash)"
+  echo "SHA1   $(cat ${PACKAGE_DIR}/sha1.hash)"
+  echo "SHA256 $(cat ${PACKAGE_DIR}/sha256.hash)"
+  echo "SHA512 $(cat ${PACKAGE_DIR}/sha512.hash)"
+
   echo "Packing ${PACKAGE_NAME}"
 	cp ${OUTPUT_BINARY} ${PACKAGE_DIR}
 	cp README.md ${PACKAGE_DIR}
 	cp LICENSE ${PACKAGE_DIR}
-	cp -r gui ${PACKAGE_DIR}
-	rm -r ${PACKAGE_DIR}/gui/.git
 
 	cd ${OUTPUT_DIR}
 	sync
@@ -47,37 +50,30 @@ echo "-------------------------------------"
 echo ""
 
 echo "-------------------------------------"
-echo "Build preparation"
-echo "Generating assets.."
-go generate
-echo "-------------------------------------"
-echo ""
-
-echo "-------------------------------------"
 echo "Building individual distributions"
 echo "Building for Linux.."
-go build -ldflags "-w -s -X main.buildNumber=${BUILD_NUMBER} -X main.buildVersion=${BUILD_VERSION}" -o ${OUTPUT_DIR}/${BINARY_NAME}
+CGO_ENABLED=1 GOOS=linux GOARCH=amd64 go build -a -ldflags "-linkmode external -extldflags '-static' -w -s -X main.buildNumber=${BUILD_NUMBER} -X main.buildVersion=${BUILD_VERSION}" -o ${OUTPUT_DIR}/${BINARY_NAME}
 sleep 1
 pack linux-amd64 ${BINARY_NAME}
 echo "-------------------------------------"
 
 echo ""
 echo "Building for Windows 64bit.."
-GOOS=windows GOARCH=amd64 go build -ldflags "-w -s -X main.buildNumber=${BUILD_NUMBER} -X main.buildVersion=${BUILD_VERSION}"  -o ${OUTPUT_DIR}/${BINARY_NAME}_x86.exe
+GOOS=windows GOARCH=amd64 go build -a -ldflags "-w -s -X main.buildNumber=${BUILD_NUMBER} -X main.buildVersion=${BUILD_VERSION}"  -o ${OUTPUT_DIR}/${BINARY_NAME}_x86.exe
 sleep 1
 pack windows-x86 ${BINARY_NAME}_x86.exe
 echo "-------------------------------------"
 
 echo ""
 echo "Building for Windows 32bit.."
-GOOS=windows GOARCH=386 go build -ldflags "-w -s -X main.buildNumber=${BUILD_NUMBER} -X main.buildVersion=${BUILD_VERSION}"  -o ${OUTPUT_DIR}/${BINARY_NAME}_i686.exe
+GOOS=windows GOARCH=386 go build -a -ldflags "-w -s -X main.buildNumber=${BUILD_NUMBER} -X main.buildVersion=${BUILD_VERSION}"  -o ${OUTPUT_DIR}/${BINARY_NAME}_i686.exe
 sleep 1
 pack windows-i686 ${BINARY_NAME}_i686.exe
 echo "-------------------------------------"
 
 echo ""
 echo "Building for OsX 64bit.."
-GOOS=darwin GOARCH=amd64 go build -ldflags "-w -s -X main.buildNumber=${BUILD_NUMBER} -X main.buildVersion=${BUILD_VERSION}" -o ${OUTPUT_DIR}/${BINARY_NAME}-darwin
+GOOS=darwin GOARCH=amd64 go build -a -ldflags "-w -s -X main.buildNumber=${BUILD_NUMBER} -X main.buildVersion=${BUILD_VERSION}" -o ${OUTPUT_DIR}/${BINARY_NAME}-darwin
 sleep 1
 pack darwin-amd64 ${BINARY_NAME}-darwin
 echo "-------------------------------------"
